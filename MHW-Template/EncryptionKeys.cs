b50aff9da2a4d86f243d29cce209b460bf134031
Gregory Brutsche <gregory@mail.brutsches.local>
2020-05-24 17:01:28 -0400

    Sort encryption keys.

--- MHW-Template/EncryptionKeys.cs
+++ MHW-Template/EncryptionKeys.cs
@@ -3,18 +3,18 @@
 namespace MHW_Template {
     public static class EncryptionKeys {
         // ReSharper disable IdentifierTypo
-        public const string PL_PARAM_KEY = "j1P15gEkgVa7NdFxiqwCPitykHctY2nZPjSaElvqb0eSwcLO1cOlTqqv"; // mParam
-        public const string OWP_DAT_KEY  = "FZoS8QLyOyeFmkdrz73P9Fh2N4NcTwy3QQPjc1YRII5KWovK6yFuU8SL"; // rOtomoToolLevelTbl also .shlp
+        public const string AEQ_KEY      = "b71AMFJuw63cUTlDt5ntSAtaAvwLKizNtapy4W0QAsC39QXPr6b78Asz"; // cRomPacketSysSyncStart
+        public const string ASKILLP_KEY  = "Nb06gpPJ9WtbO6FF1ZYqm5NbLREsCzuqAY0G25ug2Ei5XkkAtVXD5Uda"; // rASkillData askill_param.asp
+        public const string CUS_PAR_KEY  = "PCEBFfRCbwIdy6AZIoNA5lXV6FEki0yBEyW4FPXZUyWgeauqY8PYeZkM"; // rCharmData
         public const string DTT_EDA_KEY  = "Fqkpg1xx1cMlvg3AtKOCLxFgVFBwHkCbjizBRV49hWmEe5lOAaNOTm7m"; // rEmDamageAttr
         public const string DTT_EPG_KEY  = "sJV4g7d55gKnQB5nS6XJ9pZ1qZmmQwNnSbidUW1OeAhHrpPd6MKbfsrt"; // rEmPartsGroup
-        public const string MSK_KEY      = "qm7psvaMXQoay7kARXpNPcLNWqsbqcOyI4lqHtxFh26HSuE6RHNq7J4e"; // A bunch?
-        public const string ROD_INSE_KEY = "SFghFQVFJycHnypExurPwut98ZZq1cwvm7lpDpASeP4biRhstQgULzlb"; // mMoneyItemTbl
-        public const string CUS_PAR_KEY  = "PCEBFfRCbwIdy6AZIoNA5lXV6FEki0yBEyW4FPXZUyWgeauqY8PYeZkM"; // rCharmData
         public const string ITLOT_KEY    = "D7N88VEGEnRl0HEHTO0xMQkbeMb37arJF488lREp90WYojAONkLoxfMt"; // rItemData .itlot
-        public const string ASKILLP_KEY  = "Nb06gpPJ9WtbO6FF1ZYqm5NbLREsCzuqAY0G25ug2Ei5XkkAtVXD5Uda"; // rASkillData askill_param.asp
-        public const string STLST_KEY    = "ZduEVAgrFnd8jpnwHF2ur64BHcd37gEYDQwRbeBwMt6dGRK3tUwm2wx5"; // rSpecialTraceLotData // CommonParam00_st.stlst
-        public const string AEQ_KEY      = "b71AMFJuw63cUTlDt5ntSAtaAvwLKizNtapy4W0QAsC39QXPr6b78Asz"; // cRomPacketSysSyncStart
         public const string MIB_KEY      = "TZNgJfzyD2WKiuV4SglmI6oN5jP2hhRJcBwzUooyfIUTM4ptDYGjuRTP";
+        public const string MSK_KEY      = "qm7psvaMXQoay7kARXpNPcLNWqsbqcOyI4lqHtxFh26HSuE6RHNq7J4e"; // A bunch?
+        public const string OWP_DAT_KEY  = "FZoS8QLyOyeFmkdrz73P9Fh2N4NcTwy3QQPjc1YRII5KWovK6yFuU8SL"; // rOtomoToolLevelTbl also .shlp
+        public const string PL_PARAM_KEY = "j1P15gEkgVa7NdFxiqwCPitykHctY2nZPjSaElvqb0eSwcLO1cOlTqqv"; // mParam
+        public const string ROD_INSE_KEY = "SFghFQVFJycHnypExurPwut98ZZq1cwvm7lpDpASeP4biRhstQgULzlb"; // mMoneyItemTbl
+        public const string STLST_KEY    = "ZduEVAgrFnd8jpnwHF2ur64BHcd37gEYDQwRbeBwMt6dGRK3tUwm2wx5"; // rSpecialTraceLotData // CommonParam00_st.stlst
 
         // The rest are untested and/or unused.
         public const string SAVE_FILE_KEY = "xieZjoe#P2134-3zmaghgpqoe0z8$3azeq";
