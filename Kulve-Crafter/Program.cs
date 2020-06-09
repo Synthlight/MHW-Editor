7ad1dc94dd11b5d7df1500614c2439c9b0954898
Gregory Brutsche <gregory@mail.brutsches.local>
2020-06-08 21:28:04 -0400

    Update KT-Crafter for new struct system.

--- Kulve-Crafter/Program.cs
+++ Kulve-Crafter/Program.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
-using System.Text;
 using MHW_Editor.Assets;
 using MHW_Editor.Items;
 using MHW_Template.Armors;
@@ -31,37 +30,28 @@ namespace Kulve_Crafter {
 
         public static void GoEqCus() {
             File.Copy(SOURCE_FILE_EQ_CUS_BASE, TARGET_FILE_EQ_CUS_BASE, true);
-            using var fileStream = new FileStream(TARGET_FILE_EQ_CUS_BASE, FileMode.Open, FileAccess.ReadWrite);
-            using var reader     = new BinaryReader(fileStream, Encoding.Default, true);
-            fileStream.Seek(6, SeekOrigin.Begin);
-            var count = reader.ReadUInt32();
 
-            var items = new List<EqCus_Weapon>();
-
-            for (var i = 0; i < count; i++) {
-                var buff = reader.ReadBytes((int) EqCus_Base.StructSize);
-
-                items.Add(new EqCus_Weapon(buff, 0));
-            }
+            var instance = EqCus.LoadData(TARGET_FILE_EQ_CUS_BASE);
+            var items    = instance.GetStructList();
 
             foreach (WeaponType weaponType in Enum.GetValues(typeof(WeaponType))) {
-                var          i       = (ushort) items.Count;
-                EqCus_Weapon newItem = null;
+                var           i       = (ushort) items.Count;
+                EqCus.Entries newItem = null;
 
                 foreach (var kvp in DataHelper.weaponIndexNameLookup[weaponType]["eng"]) {
                     if (IsValid(kvp)) continue;
 
-                    newItem = new EqCus_Weapon(new byte[EqCus_Base.StructSize], 0) {
-                        Equipment_Category       = weaponType,
-                        Equipment_Index          = (ushort) kvp.Key,
-                        Monster_Unlock           = -1,
-                        Story_Unlock             = 1,
-                        Needed_Item_Id_to_Unlock = 888,
-                        Mat_1_Id                 = 888,
-                        Mat_1_Count              = 1,
-                        Unk_2                    = 1,
-                        Child_index_1            = ++i,
-                        Item_Rank                = CharmRankType.High_Rank
+                    newItem = new EqCus.Entries {
+                        Equipment_Category_Weapon = weaponType,
+                        Equipment_Index_Weapon    = (ushort) kvp.Key,
+                        Monster_Unlock            = -1,
+                        Story_Unlock              = 1,
+                        Needed_Item_Id_to_Unlock  = 888,
+                        Mat_1_Id                  = 888,
+                        Mat_1_Count               = 1,
+                        Unk_2                     = 1,
+                        Child_index_1             = ++i,
+                        Item_Rank                 = CharmRankType.High_Rank
                     };
                     items.Add(newItem);
                 }
@@ -69,56 +59,35 @@ namespace Kulve_Crafter {
                 if (newItem != null) newItem.Child_index_1 = 0;
             }
 
-            using var writer = new BinaryWriter(fileStream, Encoding.Default, true);
-            fileStream.Seek(6, SeekOrigin.Begin);
-            writer.Write((uint) items.Count);
-
-            foreach (var item in items) {
-                writer.Write(item.Bytes);
-            }
+            instance.SaveFile(TARGET_FILE_EQ_CUS_BASE);
         }
 
         public static void GoEqCrt() {
             File.Copy(SOURCE_FILE_EQ_CRT_BASE, TARGET_FILE_EQ_CRT_BASE, true);
-            using var fileStream = new FileStream(TARGET_FILE_EQ_CRT_BASE, FileMode.Open, FileAccess.ReadWrite);
-            using var reader     = new BinaryReader(fileStream, Encoding.Default, true);
-            fileStream.Seek(6, SeekOrigin.Begin);
-            var count = reader.ReadUInt32();
 
-            var items = new List<EqCrt_Weapon>();
-
-            for (var i = 0; i < count; i++) {
-                var buff = reader.ReadBytes((int) EqCrt_Base.StructSize);
-
-                items.Add(new EqCrt_Weapon(buff, 0));
-            }
+            var instance = EqCrt.LoadData(TARGET_FILE_EQ_CRT_BASE);
+            var items    = instance.GetStructList();
 
             foreach (WeaponType weaponType in Enum.GetValues(typeof(WeaponType))) {
                 foreach (var kvp in DataHelper.weaponIndexNameLookup[weaponType]["eng"]) {
                     if (IsValid(kvp)) continue;
 
-                    var newItem = new EqCrt_Weapon(new byte[EqCrt_Base.StructSize], 0) {
-                        Equipment_Category       = weaponType,
-                        Equipment_Index          = (ushort) kvp.Key,
-                        Monster_Unlock           = -1,
-                        Story_Unlock             = 1,
-                        Needed_Item_Id_to_Unlock = 888,
-                        Mat_1_Id                 = 888,
-                        Mat_1_Count              = 1,
-                        Unknown_uint8_2          = 1,
-                        Item_Rank = CharmRankType.High_Rank
+                    var newItem = new EqCrt.Entries {
+                        Equipment_Category_Weapon = weaponType,
+                        Equipment_Index_Weapon    = (ushort) kvp.Key,
+                        Monster_Unlock            = -1,
+                        Story_Unlock              = 1,
+                        Needed_Item_Id_to_Unlock  = 888,
+                        Mat_1_Id                  = 888,
+                        Mat_1_Count               = 1,
+                        Unknown_uint8_2           = 1,
+                        Item_Rank                 = CharmRankType.High_Rank
                     };
                     items.Add(newItem);
                 }
             }
 
-            using var writer = new BinaryWriter(fileStream, Encoding.Default, true);
-            fileStream.Seek(6, SeekOrigin.Begin);
-            writer.Write((uint) items.Count);
-
-            foreach (var item in items) {
-                writer.Write(item.Bytes);
-            }
+            instance.SaveFile(TARGET_FILE_EQ_CRT_BASE);
         }
     }
 }
