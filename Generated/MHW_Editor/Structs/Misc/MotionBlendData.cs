a048abb96de9e78ffedf7585fe13414e7cb4e1a5
Gregory Brutsche <gregory@mail.brutsches.local>
2020-12-17 14:27:42 -0500

    Fix/update mdb. (Thanks La Vie En Rose & Asterisk.)

--- Generated/MHW_Editor/Structs/Misc/MotionBlendData.cs
+++ Generated/MHW_Editor/Structs/Misc/MotionBlendData.cs
@@ -69,7 +69,22 @@ namespace MHW_Editor.Structs.Misc {
                 }
             }
 
-            public const int lastSortIndex = 200;
+            protected uint Unk_1_raw;
+            public const string Unk_1_displayName = "Unk 1";
+            public const int Unk_1_sortIndex = 200;
+            [SortOrder(Unk_1_sortIndex)]
+            [DisplayName(Unk_1_displayName)]
+            public virtual uint Unk_1 {
+                get => Unk_1_raw;
+                set {
+                    if (Unk_1_raw == value) return;
+                    Unk_1_raw = value;
+                    ChangedItems.Add(nameof(Unk_1));
+                    OnPropertyChanged(nameof(Unk_1));
+                }
+            }
+
+            public const int lastSortIndex = 250;
 
             public static ObservableMhwStructCollection<Motion_Blend_Data_1_> LoadData(BinaryReader reader) {
                 var list = new ObservableMhwStructCollection<Motion_Blend_Data_1_>();
@@ -86,6 +101,7 @@ namespace MHW_Editor.Structs.Misc {
                 data.Magic_1_raw = reader.ReadUInt32();
                 data.Magic_2_raw = reader.ReadUInt32();
                 data.Entry_Count_raw = reader.ReadUInt32();
+                data.Unk_1_raw = reader.ReadUInt32();
                 return data;
             }
 
@@ -93,6 +109,7 @@ namespace MHW_Editor.Structs.Misc {
                 writer.Write(Magic_1_raw);
                 writer.Write(Magic_2_raw);
                 writer.Write(Entry_Count_raw);
+                writer.Write(Unk_1_raw);
             }
         }
 
@@ -106,24 +123,9 @@ namespace MHW_Editor.Structs.Misc {
             [DisplayName("X")]
             public string Delete => "X";
 
-            protected uint Unk_1_raw;
-            public const string Unk_1_displayName = "Unk 1";
-            public const int Unk_1_sortIndex = 50;
-            [SortOrder(Unk_1_sortIndex)]
-            [DisplayName(Unk_1_displayName)]
-            public virtual uint Unk_1 {
-                get => Unk_1_raw;
-                set {
-                    if (Unk_1_raw == value) return;
-                    Unk_1_raw = value;
-                    ChangedItems.Add(nameof(Unk_1));
-                    OnPropertyChanged(nameof(Unk_1));
-                }
-            }
-
             protected uint lmt_Offset_Index_raw;
             public const string lmt_Offset_Index_displayName = "lmt Offset Index";
-            public const int lmt_Offset_Index_sortIndex = 100;
+            public const int lmt_Offset_Index_sortIndex = 50;
             [SortOrder(lmt_Offset_Index_sortIndex)]
             [DisplayName(lmt_Offset_Index_displayName)]
             public virtual uint lmt_Offset_Index {
@@ -138,7 +140,7 @@ namespace MHW_Editor.Structs.Misc {
 
             protected uint Num_of_Values_raw;
             public const string Num_of_Values_displayName = "Num of Values";
-            public const int Num_of_Values_sortIndex = 150;
+            public const int Num_of_Values_sortIndex = 100;
             [SortOrder(Num_of_Values_sortIndex)]
             [DisplayName(Num_of_Values_displayName)]
             [IsReadOnly]
@@ -154,10 +156,9 @@ namespace MHW_Editor.Structs.Misc {
 
             protected uint Unk_2_raw;
             public const string Unk_2_displayName = "Unk 2";
-            public const int Unk_2_sortIndex = 200;
+            public const int Unk_2_sortIndex = 150;
             [SortOrder(Unk_2_sortIndex)]
             [DisplayName(Unk_2_displayName)]
-            [IsReadOnly]
             public virtual uint Unk_2 {
                 get => Unk_2_raw;
                 set {
@@ -170,10 +171,9 @@ namespace MHW_Editor.Structs.Misc {
 
             protected uint Unk_3_raw;
             public const string Unk_3_displayName = "Unk 3";
-            public const int Unk_3_sortIndex = 250;
+            public const int Unk_3_sortIndex = 200;
             [SortOrder(Unk_3_sortIndex)]
             [DisplayName(Unk_3_displayName)]
-            [IsReadOnly]
             public virtual uint Unk_3 {
                 get => Unk_3_raw;
                 set {
@@ -209,12 +209,12 @@ namespace MHW_Editor.Structs.Misc {
                     }
                 }
 
-                protected uint Unk_2_raw;
+                protected float Unk_2_raw;
                 public const string Unk_2_displayName = "Unk 2";
                 public const int Unk_2_sortIndex = 100;
                 [SortOrder(Unk_2_sortIndex)]
                 [DisplayName(Unk_2_displayName)]
-                public virtual uint Unk_2 {
+                public virtual float Unk_2 {
                     get => Unk_2_raw;
                     set {
                         if (Unk_2_raw == value) return;
@@ -224,48 +224,48 @@ namespace MHW_Editor.Structs.Misc {
                     }
                 }
 
-                protected uint Unk_3_raw;
-                public const string Unk_3_displayName = "Unk 3";
-                public const int Unk_3_sortIndex = 150;
-                [SortOrder(Unk_3_sortIndex)]
-                [DisplayName(Unk_3_displayName)]
-                public virtual uint Unk_3 {
-                    get => Unk_3_raw;
+                protected uint Animation_Start_Frame_raw;
+                public const string Animation_Start_Frame_displayName = "Animation Start Frame";
+                public const int Animation_Start_Frame_sortIndex = 150;
+                [SortOrder(Animation_Start_Frame_sortIndex)]
+                [DisplayName(Animation_Start_Frame_displayName)]
+                public virtual uint Animation_Start_Frame {
+                    get => Animation_Start_Frame_raw;
                     set {
-                        if (Unk_3_raw == value) return;
-                        Unk_3_raw = value;
-                        ChangedItems.Add(nameof(Unk_3));
-                        OnPropertyChanged(nameof(Unk_3));
+                        if (Animation_Start_Frame_raw == value) return;
+                        Animation_Start_Frame_raw = value;
+                        ChangedItems.Add(nameof(Animation_Start_Frame));
+                        OnPropertyChanged(nameof(Animation_Start_Frame));
                     }
                 }
 
-                protected uint Unk_4_raw;
-                public const string Unk_4_displayName = "Unk 4";
-                public const int Unk_4_sortIndex = 200;
-                [SortOrder(Unk_4_sortIndex)]
-                [DisplayName(Unk_4_displayName)]
-                public virtual uint Unk_4 {
-                    get => Unk_4_raw;
+                protected uint Interpolation_raw;
+                public const string Interpolation_displayName = "Interpolation";
+                public const int Interpolation_sortIndex = 200;
+                [SortOrder(Interpolation_sortIndex)]
+                [DisplayName(Interpolation_displayName)]
+                public virtual uint Interpolation {
+                    get => Interpolation_raw;
                     set {
-                        if (Unk_4_raw == value) return;
-                        Unk_4_raw = value;
-                        ChangedItems.Add(nameof(Unk_4));
-                        OnPropertyChanged(nameof(Unk_4));
+                        if (Interpolation_raw == value) return;
+                        Interpolation_raw = value;
+                        ChangedItems.Add(nameof(Interpolation));
+                        OnPropertyChanged(nameof(Interpolation));
                     }
                 }
 
-                protected uint Unk_5_raw;
-                public const string Unk_5_displayName = "Unk 5";
-                public const int Unk_5_sortIndex = 250;
-                [SortOrder(Unk_5_sortIndex)]
-                [DisplayName(Unk_5_displayName)]
-                public virtual uint Unk_5 {
-                    get => Unk_5_raw;
+                protected uint Overlap_Blending_raw;
+                public const string Overlap_Blending_displayName = "Overlap Blending";
+                public const int Overlap_Blending_sortIndex = 250;
+                [SortOrder(Overlap_Blending_sortIndex)]
+                [DisplayName(Overlap_Blending_displayName)]
+                public virtual uint Overlap_Blending {
+                    get => Overlap_Blending_raw;
                     set {
-                        if (Unk_5_raw == value) return;
-                        Unk_5_raw = value;
-                        ChangedItems.Add(nameof(Unk_5));
-                        OnPropertyChanged(nameof(Unk_5));
+                        if (Overlap_Blending_raw == value) return;
+                        Overlap_Blending_raw = value;
+                        ChangedItems.Add(nameof(Overlap_Blending));
+                        OnPropertyChanged(nameof(Overlap_Blending));
                     }
                 }
 
@@ -284,18 +284,18 @@ namespace MHW_Editor.Structs.Misc {
                     }
                 }
 
-                protected uint Unk_7_raw;
-                public const string Unk_7_displayName = "Unk 7";
-                public const int Unk_7_sortIndex = 350;
-                [SortOrder(Unk_7_sortIndex)]
-                [DisplayName(Unk_7_displayName)]
-                public virtual uint Unk_7 {
-                    get => Unk_7_raw;
+                protected uint Interpolation_Modifier__raw;
+                public const string Interpolation_Modifier__displayName = "Interpolation Modifier?";
+                public const int Interpolation_Modifier__sortIndex = 350;
+                [SortOrder(Interpolation_Modifier__sortIndex)]
+                [DisplayName(Interpolation_Modifier__displayName)]
+                public virtual uint Interpolation_Modifier_ {
+                    get => Interpolation_Modifier__raw;
                     set {
-                        if (Unk_7_raw == value) return;
-                        Unk_7_raw = value;
-                        ChangedItems.Add(nameof(Unk_7));
-                        OnPropertyChanged(nameof(Unk_7));
+                        if (Interpolation_Modifier__raw == value) return;
+                        Interpolation_Modifier__raw = value;
+                        ChangedItems.Add(nameof(Interpolation_Modifier_));
+                        OnPropertyChanged(nameof(Interpolation_Modifier_));
                     }
                 }
 
@@ -314,18 +314,18 @@ namespace MHW_Editor.Structs.Misc {
                     }
                 }
 
-                protected uint Unk_9_raw;
-                public const string Unk_9_displayName = "Unk 9";
-                public const int Unk_9_sortIndex = 450;
-                [SortOrder(Unk_9_sortIndex)]
-                [DisplayName(Unk_9_displayName)]
-                public virtual uint Unk_9 {
-                    get => Unk_9_raw;
+                protected uint Animation_Id_raw;
+                public const string Animation_Id_displayName = "Animation Id";
+                public const int Animation_Id_sortIndex = 450;
+                [SortOrder(Animation_Id_sortIndex)]
+                [DisplayName(Animation_Id_displayName)]
+                public virtual uint Animation_Id {
+                    get => Animation_Id_raw;
                     set {
-                        if (Unk_9_raw == value) return;
-                        Unk_9_raw = value;
-                        ChangedItems.Add(nameof(Unk_9));
-                        OnPropertyChanged(nameof(Unk_9));
+                        if (Animation_Id_raw == value) return;
+                        Animation_Id_raw = value;
+                        ChangedItems.Add(nameof(Animation_Id));
+                        OnPropertyChanged(nameof(Animation_Id));
                     }
                 }
 
@@ -344,42 +344,41 @@ namespace MHW_Editor.Structs.Misc {
                     var data = new Unk_Array();
                     data.Index = i;
                     data.Unk_1_raw = reader.ReadNullTermString();
-                    data.Unk_2_raw = reader.ReadUInt32();
-                    data.Unk_3_raw = reader.ReadUInt32();
-                    data.Unk_4_raw = reader.ReadUInt32();
-                    data.Unk_5_raw = reader.ReadUInt32();
+                    data.Unk_2_raw = reader.ReadSingle();
+                    data.Animation_Start_Frame_raw = reader.ReadUInt32();
+                    data.Interpolation_raw = reader.ReadUInt32();
+                    data.Overlap_Blending_raw = reader.ReadUInt32();
                     data.Unk_6_raw = reader.ReadSingle();
-                    data.Unk_7_raw = reader.ReadUInt32();
+                    data.Interpolation_Modifier__raw = reader.ReadUInt32();
                     data.Unk_8_raw = reader.ReadInt32();
-                    data.Unk_9_raw = reader.ReadUInt32();
+                    data.Animation_Id_raw = reader.ReadUInt32();
                     return data;
                 }
 
                 public void WriteData(BinaryWriter writer, Default parent) {
                     writer.Write(Unk_1_raw.ToNullTermCharArray());
                     writer.Write(Unk_2_raw);
-                    writer.Write(Unk_3_raw);
-                    writer.Write(Unk_4_raw);
-                    writer.Write(Unk_5_raw);
+                    writer.Write(Animation_Start_Frame_raw);
+                    writer.Write(Interpolation_raw);
+                    writer.Write(Overlap_Blending_raw);
                     writer.Write(Unk_6_raw);
-                    writer.Write(Unk_7_raw);
+                    writer.Write(Interpolation_Modifier__raw);
                     writer.Write(Unk_8_raw);
-                    writer.Write(Unk_9_raw);
+                    writer.Write(Animation_Id_raw);
                 }
             }
 
             public const string Unk_4_displayName = "Unk 4";
-            public const int Unk_4_sortIndex = 300;
+            public const int Unk_4_sortIndex = 250;
             [SortOrder(Unk_4_sortIndex)]
             [DisplayName(Unk_4_displayName)]
             public virtual ObservableCollection<Unk_Array> Unk_4_raw { get; protected set; }
 
             protected uint Unk_5_raw;
             public const string Unk_5_displayName = "Unk 5";
-            public const int Unk_5_sortIndex = 350;
+            public const int Unk_5_sortIndex = 300;
             [SortOrder(Unk_5_sortIndex)]
             [DisplayName(Unk_5_displayName)]
-            [IsReadOnly]
             public virtual uint Unk_5 {
                 get => Unk_5_raw;
                 set {
@@ -387,6 +386,21 @@ namespace MHW_Editor.Structs.Misc {
                     Unk_5_raw = value;
                     ChangedItems.Add(nameof(Unk_5));
                     OnPropertyChanged(nameof(Unk_5));
+                }
+            }
+
+            protected uint Unk_6_raw;
+            public const string Unk_6_displayName = "Unk 6";
+            public const int Unk_6_sortIndex = 350;
+            [SortOrder(Unk_6_sortIndex)]
+            [DisplayName(Unk_6_displayName)]
+            public virtual uint Unk_6 {
+                get => Unk_6_raw;
+                set {
+                    if (Unk_6_raw == value) return;
+                    Unk_6_raw = value;
+                    ChangedItems.Add(nameof(Unk_6));
+                    OnPropertyChanged(nameof(Unk_6));
                 }
             }
 
@@ -413,19 +427,18 @@ namespace MHW_Editor.Structs.Misc {
             public static Default LoadData(BinaryReader reader, ulong i) {
                 var data = new Default();
                 data.Index = i;
-                data.Unk_1_raw = reader.ReadUInt32();
                 data.lmt_Offset_Index_raw = reader.ReadUInt32();
                 data.Num_of_Values_raw = reader.ReadUInt32();
                 data.Unk_2_raw = reader.ReadUInt32();
                 data.Unk_3_raw = reader.ReadUInt32();
                 data.Unk_4_raw = Unk_Array.LoadData(reader, data);
                 data.Unk_5_raw = reader.ReadUInt32();
+                data.Unk_6_raw = reader.ReadUInt32();
                 return data;
             }
 
             public void WriteData(BinaryWriter writer) {
                 Num_of_Values = (uint) Unk_4_raw.Count;
-                writer.Write(Unk_1_raw);
                 writer.Write(lmt_Offset_Index_raw);
                 writer.Write(Num_of_Values_raw);
                 writer.Write(Unk_2_raw);
@@ -434,48 +447,7 @@ namespace MHW_Editor.Structs.Misc {
                     obj.WriteData(writer, this);
                 }
                 writer.Write(Unk_5_raw);
-            }
-        }
-
-        public partial class Motion_Blend_Data_2_ : MhwStructItem, IWriteData {
-            public const ulong FixedSizeCount = 1;
-            public const string GridName = "Motion Blend Data (2)";
-
-            protected uint End_raw;
-            public const string End_displayName = "End";
-            public const int End_sortIndex = 50;
-            [SortOrder(End_sortIndex)]
-            [DisplayName(End_displayName)]
-            public virtual uint End {
-                get => End_raw;
-                set {
-                    if (End_raw == value) return;
-                    End_raw = value;
-                    ChangedItems.Add(nameof(End));
-                    OnPropertyChanged(nameof(End));
-                }
-            }
-
-            public const int lastSortIndex = 100;
-
-            public static ObservableMhwStructCollection<Motion_Blend_Data_2_> LoadData(BinaryReader reader) {
-                var list = new ObservableMhwStructCollection<Motion_Blend_Data_2_>();
-                const ulong count = 1UL;
-                for (ulong i = 0; i < count; i++) {
-                    list.Add(LoadData(reader, i));
-                }
-                return list;
-            }
-
-            public static Motion_Blend_Data_2_ LoadData(BinaryReader reader, ulong i) {
-                var data = new Motion_Blend_Data_2_();
-                data.Index = i;
-                data.End_raw = reader.ReadUInt32();
-                return data;
-            }
-
-            public void WriteData(BinaryWriter writer) {
-                writer.Write(End_raw);
+                writer.Write(Unk_6_raw);
             }
         }
 
@@ -487,8 +459,6 @@ namespace MHW_Editor.Structs.Misc {
             var Default_ = new MhwStructDataContainer<Default, Motion_Blend_Data_1_>(Default.LoadData(reader, Motion_Blend_Data_1__.list), typeof(Default));
             Default_.SetCountTargetToUpdate(Motion_Blend_Data_1__, -1, "Entry_Count");
             data.AddLast(Default_);
-            var Motion_Blend_Data_2__ = new MhwStructDataContainer<Motion_Blend_Data_2_>(Motion_Blend_Data_2_.LoadData(reader), typeof(Motion_Blend_Data_2_));
-            data.AddLast(Motion_Blend_Data_2__);
         }
     }
 }
--- MHW-Generator/Misc/MotionBlendData.cs
+++ MHW-Generator/Misc/MotionBlendData.cs
@@ -9,34 +9,31 @@ namespace MHW_Generator.Misc {
                 new MhwMultiStructData.StructData("Motion Blend Data (1)", new List<MhwMultiStructData.Entry> {
                     new MhwMultiStructData.Entry("Magic 1", typeof(uint), true),
                     new MhwMultiStructData.Entry("Magic 2", typeof(uint), true),
-                    new MhwMultiStructData.Entry("Entry Count", typeof(uint), true).Out(out var entryCount)
+                    new MhwMultiStructData.Entry("Entry Count", typeof(uint), true).Out(out var entryCount),
+                    new MhwMultiStructData.Entry("Unk 1", typeof(uint))
                 }, 1).Out(out var header),
 
                 new MhwMultiStructData.StructData("Default", new List<MhwMultiStructData.Entry> {
-                    new MhwMultiStructData.Entry("Unk 1", typeof(uint)),
                     new MhwMultiStructData.Entry("lmt Offset Index", typeof(uint)),
                     new MhwMultiStructData.Entry("Num of Values", typeof(uint), true).Out(out var innerCount),
-                    new MhwMultiStructData.Entry("Unk 2", typeof(uint), true),
-                    new MhwMultiStructData.Entry("Unk 3", typeof(uint), true),
+                    new MhwMultiStructData.Entry("Unk 2", typeof(uint)),
+                    new MhwMultiStructData.Entry("Unk 3", typeof(uint)),
 
                     new MhwMultiStructData.Entry("Unk 4", typeof(void), subStruct: new MhwMultiStructData.StructData("Unk Array", new List<MhwMultiStructData.Entry> {
                         new MhwMultiStructData.Entry("Unk 1", typeof(string), isNullTerminatedString: true),
-                        new MhwMultiStructData.Entry("Unk 2", typeof(uint)),
-                        new MhwMultiStructData.Entry("Unk 3", typeof(uint)),
-                        new MhwMultiStructData.Entry("Unk 4", typeof(uint)),
-                        new MhwMultiStructData.Entry("Unk 5", typeof(uint)),
+                        new MhwMultiStructData.Entry("Unk 2", typeof(float)),
+                        new MhwMultiStructData.Entry("Animation Start Frame", typeof(uint)),
+                        new MhwMultiStructData.Entry("Interpolation", typeof(uint)),
+                        new MhwMultiStructData.Entry("Overlap Blending", typeof(uint)),
                         new MhwMultiStructData.Entry("Unk 6", typeof(float)),
-                        new MhwMultiStructData.Entry("Unk 7", typeof(uint)),
+                        new MhwMultiStructData.Entry("Interpolation Modifier?", typeof(uint)),
                         new MhwMultiStructData.Entry("Unk 8", typeof(int)),
-                        new MhwMultiStructData.Entry("Unk 9", typeof(uint))
+                        new MhwMultiStructData.Entry("Animation Id", typeof(uint))
                     }, canAddRows: true, _010Link: new MhwMultiStructData.ArrayLink(null, innerCount))),
 
-                    new MhwMultiStructData.Entry("Unk 5", typeof(uint), true)
-                }, canAddRows: true, _010Link: new MhwMultiStructData.ArrayLink(header, entryCount)),
-
-                new MhwMultiStructData.StructData("Motion Blend Data (2)", new List<MhwMultiStructData.Entry> {
-                    new MhwMultiStructData.Entry("End", typeof(uint))
-                }, 1)
+                    new MhwMultiStructData.Entry("Unk 5", typeof(uint)),
+                    new MhwMultiStructData.Entry("Unk 6", typeof(uint))
+                }, canAddRows: true, _010Link: new MhwMultiStructData.ArrayLink(header, entryCount))
             };
 
             return new MultiStruct("Misc", "MotionBlendData", new MhwMultiStructData(structs, "mbd"));
